021dcbdc3fa617047705eb5ad69c4c1665380473
Mauro9735 <mauriciovilladiego1999@gmail.com>
2025-05-04 18:15:51 -0500

    Agregar configuracion rabbit serializar objetos

--- src/main/java/com/marcianos/bridge_transfer_citizen/BridgeTransferCitizenApplication.java
+++ src/main/java/com/marcianos/bridge_transfer_citizen/BridgeTransferCitizenApplication.java
@@ -2,8 +2,9 @@ package com.marcianos.bridge_transfer_citizen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class BridgeTransferCitizenApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/marcianos/bridge_transfer_citizen/dto/RequestTransferCitizenBridge.java
+++ src/main/java/com/marcianos/bridge_transfer_citizen/dto/RequestTransferCitizenBridge.java
@@ -1,15 +1,23 @@
 package com.marcianos.bridge_transfer_citizen.dto;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
+
+import java.io.Serializable;
 
 @Builder
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class RequestTransferCitizenBridge {
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class RequestTransferCitizenBridge implements Serializable {
+    @JsonProperty("operatorId")
     private String operatorId;
+    @JsonProperty("operatorName")
     private String operatorName;
+    @JsonProperty("transferUrl")
     private String transferUrl;
 }
